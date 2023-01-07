a6f47de067f1ed01af361c6ff0e3ec6c98a12b21
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2023-01-07 18:43:27 +0100

    Initial commit

---
+++ src/main/java/com/yobombel/fatlossnews/FatLossNewsApplication.java
@@ -0,0 +1,13 @@
+package com.yobombel.fatlossnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatLossNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatLossNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yobombel/fatlossnews/FatLossNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobombel.fatlossnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatLossNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
